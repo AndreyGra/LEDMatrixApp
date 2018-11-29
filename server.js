f2915d23a746159fdedc027f9065c905080d2bd3
AndreyGra <andrey@chabunin.com>
2018-11-29 15:42:47 +1100

    Forgot to start a server

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ var bodyParser = require("body-parser");
 var app = express();
 app.use(bodyParser.json());
 
+var server = app.listen(process.env.PORT || 8080, function () {
+  var port = server.address().port;
+  console.log("App now running on port", port);
+});
+
 // Create link to Angular build directory
 var distDir = __dirname + "/dist/";
 app.use(express.static(distDir));
